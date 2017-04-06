fc7b068657ed0ddbb03604b46ed735bd16211839
Michael Ringquist <mringqui@wlgore.com>
2017-04-06 17:20:44 -0400

    broken - fix data frame create
    to do:
    - fix error handling when site returns no data
    - fix handling when no history file is present:

--- sxm_scrape.py
+++ sxm_scrape.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import pandas as pd
+import datetime
+
+##Set value to 1 if first time running on this machine (no history file to start with)
+FIRSTRUN = 1
 
 #Initialize web scraping
 driver = webdriver.PhantomJS('./phantomjs-2.1.1-windows/bin/phantomjs.exe')
@@ -14,22 +18,37 @@ def get_html (url):
 
 def extract_song (soup): #Get current song title, artist, and albumart from siriusxm website
     song = {}
-    song["title"] = soup.find_all(class_="onair-pdt-song")[0].contents[0]
-    song["artist"] = soup.find_all(class_="onair-pdt-artist")[0].contents[0]
-    for pic in soup.findAll('img'):
-        if pic.parent.get('id') == 'onair-pdt':
-            song["albumart"] = pic.get('src')
-    return song["title"], song["artist"], song["albumart"]
+    song["Date"] = datetime.date
+    song["Time"] = datetime.time
+    try:
+        song["Title"] = soup.find_all(class_="onair-pdt-song")[0].contents[0]
+        song["Artist"] = soup.find_all(class_="onair-pdt-artist")[0].contents[0]
+        for pic in soup.findAll('img'):
+            if pic.parent.get('id') == 'onair-pdt':
+                song["Album Art URL"] = pic.get('src')
+        return song
+    except IndexError:
+        print("no song playing...")
+        return
 
     
 def main():
-    bpm_history = pd.DataFrame(columns=["Title", "Artist", "Album Art URL"])
-    url = 'http://www.siriusxm.com/bpm'
+    url = "http://www.siriusxm.com/bpm"
     bpm_soup = get_html(url)
-    a,b,c = extract_song(bpm_soup)
-    bpm_history = bpm_history.append({"Title": a, "Artist": b, "Album Art URL": c}, ignore_index = True)
-    print(bpm_history)
-    bpm_history.to_csv(r'c:\users\mringqui\desktop\bpm_history.csv')
+    song = extract_song(bpm_soup) ##TODO: Fix error handling
+    
+    if FIRSTRUN == 1:
+        bpm_history = pd.DataFrame(list(song.items()), columns = ["Date", "Time", "Title", "Artist", "Album Art URL"], index=[0])
+        print("Table Created")
+        FIRSTRUN == 0
+    else:
+        bpm_history = pd.read_csv(r"c:\users\mringqui\desktop\bpm_history.csv",index_col=0)
+        rows = len(bpm_history.index)
+        if not(bpm_history.to_dict('records')[rows-1] == song):
+            bpm_history = bpm_history.append(song, ignore_index = True)
+            print("Row Added")
+    
+    bpm_history.to_csv(r"c:\users\mringqui\desktop\bpm_history.csv")
     return;
 
 main()
