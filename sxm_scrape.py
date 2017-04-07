4942af3dc27d5d132333b2cf474d01e9fa043dfa
Michael Ringquist <mringqui@wlgore.com>
2017-04-07 01:04:03 -0400

    V1.0 Working Loop & Play History Log for BPM, EA

--- sxm_scrape.py
+++ sxm_scrape.py
@@ -4,34 +4,58 @@ from pathlib import Path
 
 import pandas as pd
 import datetime
+import sched
+import time
 
 #Initialize web scraping
 driver = webdriver.PhantomJS('./phantomjs-2.1.1-windows/bin/phantomjs.exe')
+driver.implicitly_wait(10)
 
 #Set sites to scrape
 BPM = 1
 ELECTRIC_AREA = 1
 CLUB_LIFE = 0
 
+
 def get_html (url):
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     return soup;
 
-
+    
+def set_channels():
+    channels = []
+    urls = []
+    history_paths = []
+    if BPM == 1:
+        channels.append("BPM")
+        urls.append("http://www.siriusxm.com/bpm")
+        history_paths.append(r"c:\users\mringqui\desktop\bpm_history.dat")
+    if ELECTRIC_AREA == 1:
+        channels.append("Electric Area")
+        urls.append("http://www.siriusxm.com/electricarea")
+        history_paths.append(r"c:\users\mringqui\desktop\electric_area_history.dat")
+    if CLUB_LIFE == 1:
+        channels.append("Tiësto's Club Life")
+        urls.append("http://www.siriusxm.com/tiesto")
+        history_paths.append(r"c:\users\mringqui\desktop\club_life_history.dat")
+    return channels, urls, history_paths
+    
+    
 def extract_song (soup): #Get current song title, artist, and albumart from siriusxm website
     song = {}
     song["Date"] = datetime.date.today().strftime("%Y-%m-%d")
-    song["Time"] = datetime.date.today().strftime("%H:%M:%S")
+    song["Time"] = time.strftime("%H:%M:%S")
     try:
         song["Title"] = soup.find_all(class_="onair-pdt-song")[0].contents[0]
         song["Artist"] = soup.find_all(class_="onair-pdt-artist")[0].contents[0]
         for pic in soup.findAll('img'):
             if pic.parent.get('id') == 'onair-pdt':
                 song["Album Art URL"] = pic.get('src')
+        print("Currently playing: ", song["Title"], " by ", song["Artist"])
         return song
     except IndexError:
-        print("No song playing...")
+        print("Could not get song...")
         return;
 
     
@@ -40,11 +64,11 @@ def open_history(url, path):
     
     if test_path.is_file():
         history = pd.read_csv(path,index_col=0)
-        rows = len(history.index)
         print("Table Loaded from", path)
+        return history;
     else:
-        bpm_soup = get_html(url)
-        song = extract_song(bpm_soup)
+        soup = get_html(url)
+        song = extract_song(soup)
         if song is not None:
             history = pd.DataFrame(song, columns = ["Artist", "Title", "Album Art URL", "Date", "Time"], index = [0])
             history.to_csv(path)
@@ -53,32 +77,37 @@ def open_history(url, path):
         else:
             print("Table NOT Created (no song playing).")
             return;
+
             
-# if not(bpm_history.to_dict('records')[rows-1] == song):
-# bpm_history = bpm_history.append(song, ignore_index = True)
-# print("Row Added")
-    
+def add_song(history_table,url,path):
+    soup = get_html(url)
+    song = extract_song(soup)
+    rows = len(history_table.index) 
+    if song is not None:
+        if not(song["Title"] == history_table["Title"][rows-1]):
+            history_table = history_table.append(song, ignore_index = True)
+            history_table.to_csv(path)
+            print("Row Added")
+    return history_table
 
-
-def set_paths():
-    urls = []
-    history_paths = []
-    if BPM == 1:
-        urls.append("http://www.siriusxm.com/bpm")
-        history_paths.append(r"c:\users\mringqui\desktop\bpm_history.dat")
-    if ELECTRIC_AREA == 1:
-        urls.append("http://www.siriusxm.com/electricarea")
-        history_paths.append(r"c:\users\mringqui\desktop\electric_area_history.dat")
-    if CLUB_LIFE == 1:
-        urls.append("http://www.siriusxm.com/tiesto")
-        history_paths.append(r"c:\users\mringqui\desktop\club_life_history.dat")
-    return urls, history_paths
     
 def main():
-    urls, history_paths = set_paths()
-    for url,path in zip(urls,history_paths):
-        history = open_history(url, path)
+    history = []
+    channels, urls, history_paths = set_channels()
     
+    for url,path in zip(urls,history_paths):
+        history.append(open_history(url, path))
+    
+    try:
+        while True:
+            for i in range(len(history)):
+                print("\nChecking", channels[i], flush=True)
+                history[i] = add_song(history[i],urls[i],history_paths[i])
+                print(channels[i], "checked at", time.strftime("%H:%M:%S"), flush=True)
+            time.sleep(120)
+    except KeyboardInterrupt:
+        print('Excecution ended by user.', flush=True)
+        driver.quit()
     return;
 
 main()
